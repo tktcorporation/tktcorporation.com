74989d9fdb7f8596aa8a5bf38f24dffc8ba3918e
tkt <37575408+tktcorporation@users.noreply.github.com>
2022-06-04 23:54:04 +0000

    feat: create base contents

--- src/about.tsx
+++ src/about.tsx
@@ -1,85 +1,94 @@
 export const About = () => {
   return (
-    <div className="container">
-      <h1>Hi 👋, I'm tkt.</h1>
-      <h3>I'm a web application developer from Japan.</h3>
+    <div className="">
+      <h1 className="text-3xl font-bold">Hi 👋, I'm tkt.</h1>
+      <h3 className="text-xl">I'm a web application developer from Japan.</h3>
 
       <div>
-        {" "}
         <a href="https://twitter.com/tktcorporation" target="blank">
           <img
             src="https://img.shields.io/twitter/follow/tktcorporation?logo=twitter&style=for-the-badge"
             alt="tktcorporation"
           />
-        </a>{" "}
+        </a>
       </div>
 
-      <h3>Connect with me:</h3>
+      <h3 className="text-xl">Connect with me:</h3>
 
-      <div>
+      <div className="flex flex-row">
         <a href="https://lapras.com/public/BKFXEQC" target="blank">
           <img
+          className="h-10 w-10 md:h-16 md:w-16 mr-2"
             src="https://assets.lapras.com/static/assets/bundles/media/logo-symbol.5c8467f1.svg"
             alt="tktcorporation"
           />
         </a>
         <a href="https://github.com/tktcorporation" target="blank">
           <img
+          className="h-10 w-10 md:h-16 md:w-16 mr-2"
             src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/github.svg"
             alt="tktcorporation"
           />
         </a>
         <a href="https://twitter.com/tktcorporation" target="blank">
           <img
+          className="h-10 w-10 md:h-16 md:w-16 mr-2"
             src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/twitter.svg"
             alt="tktcorporation"
           />
         </a>
         <a href="https://qiita.com/tktcorporation" target="blank">
           <img
+          className="h-10 w-10 md:h-16 md:w-16 mr-2"
             src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/qiita.svg"
             alt="tktcorporation"
           />
         </a>
         <a href="https://zenn.dev/tktcorporation" target="blank">
           <img
+          className="h-10 w-10 md:h-16 md:w-16 mr-2"
             src="https://simpleicons.org/icons/zenn.svg"
             alt="tktcorporation"
           />
         </a>
         <a href="https://stackoverflow.com/users/12852199" target="blank">
           <img
+          className="h-10 w-10 md:h-16 md:w-16 mr-2"
             src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/stackoverflow.svg"
             alt="tktcorporation"
           />
         </a>
         <a href="https://instagram.com/tktcorporation" target="blank">
           <img
+          className="h-10 w-10 md:h-16 md:w-16 mr-2"
             src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/instagram.svg"
             alt="tktcorporation"
           />
         </a>
         <a href="https://medium.com/@tktcorporation" target="blank">
           <img
+          className="h-10 w-10 md:h-16 md:w-16 mr-2"
             src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/medium.svg"
             alt="@tktcorporation"
           />
         </a>
         <a href="https://dev.to/tktcorporation" target="blank">
           <img
+          className="h-10 w-10 md:h-16 md:w-16 mr-2"
             src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/dev-dot-to.svg"
             alt="tktcorporation"
           />
         </a>
         <a href="https://linkedin.com/in/tktcorporation" target="blank">
           <img
+          className="h-10 w-10 md:h-16 md:w-16 mr-2"
             src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/linkedin.svg"
             alt="tktcorporation"
           />
         </a>
       </div>
 
-      <h3>Support:</h3>
+      <h3 className="text-xl">Support:</h3>
       <div>
         <a href="https://www.buymeacoffee.com/tktcorporation">
           {" "}
