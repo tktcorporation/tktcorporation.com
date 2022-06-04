f6893079dcb348077170ac6f7fae99ba4bc8948f
tkt <37575408+tktcorporation@users.noreply.github.com>
2022-06-04 18:39:00 +0000

    feat: start to recreate all pages

---
+++ src/about.tsx
@@ -0,0 +1,95 @@
+export const About = () => {
+  return (
+    <div className="container">
+      <h1>Hi 👋, I'm tkt.</h1>
+      <h3>I'm a web application developer from Japan.</h3>
+
+      <div>
+        {" "}
+        <a href="https://twitter.com/tktcorporation" target="blank">
+          <img
+            src="https://img.shields.io/twitter/follow/tktcorporation?logo=twitter&style=for-the-badge"
+            alt="tktcorporation"
+          />
+        </a>{" "}
+      </div>
+
+      <h3>Connect with me:</h3>
+
+      <div>
+        <a href="https://lapras.com/public/BKFXEQC" target="blank">
+          <img
+            src="https://assets.lapras.com/static/assets/bundles/media/logo-symbol.5c8467f1.svg"
+            alt="tktcorporation"
+          />
+        </a>
+        <a href="https://github.com/tktcorporation" target="blank">
+          <img
+            src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/github.svg"
+            alt="tktcorporation"
+          />
+        </a>
+        <a href="https://twitter.com/tktcorporation" target="blank">
+          <img
+            src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/twitter.svg"
+            alt="tktcorporation"
+          />
+        </a>
+        <a href="https://qiita.com/tktcorporation" target="blank">
+          <img
+            src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/qiita.svg"
+            alt="tktcorporation"
+          />
+        </a>
+        <a href="https://zenn.dev/tktcorporation" target="blank">
+          <img
+            src="https://simpleicons.org/icons/zenn.svg"
+            alt="tktcorporation"
+          />
+        </a>
+        <a href="https://stackoverflow.com/users/12852199" target="blank">
+          <img
+            src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/stackoverflow.svg"
+            alt="tktcorporation"
+          />
+        </a>
+        <a href="https://instagram.com/tktcorporation" target="blank">
+          <img
+            src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/instagram.svg"
+            alt="tktcorporation"
+          />
+        </a>
+        <a href="https://medium.com/@tktcorporation" target="blank">
+          <img
+            src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/medium.svg"
+            alt="@tktcorporation"
+          />
+        </a>
+        <a href="https://dev.to/tktcorporation" target="blank">
+          <img
+            src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/dev-dot-to.svg"
+            alt="tktcorporation"
+          />
+        </a>
+        <a href="https://linkedin.com/in/tktcorporation" target="blank">
+          <img
+            src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/linkedin.svg"
+            alt="tktcorporation"
+          />
+        </a>
+      </div>
+
+      <h3>Support:</h3>
+      <div>
+        <a href="https://www.buymeacoffee.com/tktcorporation">
+          {" "}
+          <img
+            src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+            alt="tktcorporation"
+            height="100"
+          />
+        </a>
+      </div>
+    </div>
+  );
+};
