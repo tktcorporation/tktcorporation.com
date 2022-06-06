d2fbd04bfc2790ee7582569b513016d057b68916
tkt <37575408+tktcorporation@users.noreply.github.com>
2022-06-06 19:06:20 +0000

    feat: firebase analytics setttings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { analytics } from '../firebase'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // firebase
+  useEffect(() => {
+    analytics.app.automaticDataCollectionEnabled = true
+  }, [])
+
   return <Component {...pageProps} />
 }
 
