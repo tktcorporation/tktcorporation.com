262c8e7b329659d72a484f489803026981a0d0e5
tktcorporation <tktcorporation@users.noreply.github.com>
2022-08-02 14:40:34 +0000

    deploy: 1814a9cfc2bcad0de1e637a6b99173e7701fdb32

--- _next/static/Kakc5v11oRhj4oI3C9elQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/129-f47d8e15aa30648d.js","static/css/31e443d25a4bf398.css","static/chunks/pages/index-e8b750791dab9118.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
