1814a9cfc2bcad0de1e637a6b99173e7701fdb32
tkt <37575408+tktcorporation@users.noreply.github.com>
2022-08-02 14:38:59 +0000

    feat: use next-image-export-optimizer

--- src/about.tsx
+++ src/about.tsx
@@ -1,3 +1,5 @@
+import ExportedImage from "next-image-export-optimizer";
+
 export const About = () => {
   return (
     <div className="">
@@ -79,8 +81,10 @@ export const About = () => {
       <h3 className="text-3xl mt-16">Support:</h3>
       <div className="mt-8">
       <a href="https://www.buymeacoffee.com/tktcorporation">
-          <img
+          <ExportedImage
             className="w-64"
+            height={72}
+            width={272}
             src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
             alt="buymeacoffee for tktcorporation"
           />
@@ -101,12 +105,14 @@ const LinkIcon = ({
 }) => {
   return (
     <a href={href} target="blank" className={className}>
-      <img
+      <ExportedImage
         className="h-10 w-10 md:h-16 md:w-16"
         style={{
           filter:
             "invert(88%) sepia(61%) saturate(0%) hue-rotate(229deg) brightness(107%) contrast(101%)",
         }}
+        width={64}
+        height={64}
         src={src}
         alt={href}
       />
