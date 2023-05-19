9492c1d8fd133c3430f2f5ac8c0ce3c983803244
tktcorporation <tktcorporation@users.noreply.github.com>
2023-05-19 22:19:51 +0000

    deploy: 227fd5d1951e4654c0310a3c1ed02be3ceedfcab

---
+++ _next/static/xjs0EiKWa-nANwSnqrl_J/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/31e443d25a4bf398.css","static/chunks/pages/index-e4dfad01d0959998.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
